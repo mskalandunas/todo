8d8149b9ff444a166532c1d34c7076e40a49c110
Mike Skalandunas <mskalandunas@gmail.com>
2021-07-02 15:41:14 -0700

    refactor interfaces

--- client/components/interfaces.js
+++ client/components/interfaces.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 export const ICard = {
   body: PropTypes.string.isRequired,
   heading: PropTypes.string.isRequired,
-  media: PropTypes.shape({
-    imageURL: PropTypes.string,
-    externalURL: PropTypes.string
-  }),
-  subtasks: PropTypes.shape({
-    meta: PropTypes.shape({
-      count: PropTypes.number.isRequired
-    }).isRequired,
-    items: PropTypes.arrayOf(PropTypes.string).isRequired
-  }).isRequired  
+  media: IMedia,
+  subtasks: ISubtasks
 };
+
+const IMedia = PropTypes.shape({
+  imageURL: PropTypes.string,
+  externalURL: PropTypes.string
+});
+
+const ISubtasks = PropTypes.shape({
+  meta: PropTypes.shape({
+    count: PropTypes.number.isRequired
+  }).isRequired,
+  items: PropTypes.arrayOf(PropTypes.string).isRequired
+}).isRequired;

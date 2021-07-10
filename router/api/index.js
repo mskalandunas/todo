c6164c7a88a63827a6c88ddc6337f45a7d5c84be
Mike Skalandunas <mskalandunas@gmail.com>
2021-07-10 13:53:57 -0700

    simplify router files

--- router/api/index.js
+++ router/api/index.js
@@ -2,8 +2,5 @@ import { Router } from 'express';
 
 import { itemRouter } from './item';
 
-const apiRouter = Router();
-
-apiRouter.use('/item', itemRouter);
-
-export { apiRouter };
+export const apiRouter = Router()
+  .use('/item', itemRouter);
--- router/index.js
+++ router/index.js
@@ -3,10 +3,6 @@ import { Router } from 'express';
 import { apiRouter } from './api';
 import { viewRouter } from './views';
 
-const baseRouter = Router();
-
-baseRouter
+export const baseRouter = Router()
   .use('/', viewRouter)
   .use('/api', apiRouter);
-
-export { baseRouter };
